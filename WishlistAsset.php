8ff9b8de239123ac4e214c25489ac938a360749c
Andrew <andrew.panix@gmail.com>
2019-04-29 04:04:24 +0300

    init

---
+++ WishlistAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace panix\mod\wishlist;
+
+use panix\engine\web\AssetBundle;
+
+class WishlistAsset extends AssetBundle
+{
+
+    public $sourcePath = __DIR__.'/assets';
+
+    public $js = [
+        'js/wishlist.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
