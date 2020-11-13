cc1caae468164ae0810c9ffd6454d8d002eb839a
Andrew <andrew.panix@gmail.com>
2020-11-13 22:21:46 +0200

    init

--- migrations/m170910_040208_wishlist.php
+++ migrations/m170910_040208_wishlist.php
@@ -18,15 +18,15 @@ class m170910_040208_wishlist extends Migration
         $this->createTable(WishList::tableName(), [
             'id' => $this->primaryKey(),
             'key' => $this->string(10)->defaultValue(''),
-            'user_id' => $this->integer(),
+            'user_id' => $this->integer()->unsigned(),
         ], $this->tableOptions);
 
 
         $this->createTable(WishListProducts::tableName(), [
             'id' => $this->primaryKey(),
             'wishlist_id' => $this->integer(),
-            'product_id' => $this->integer(),
-            'user_id' => $this->integer(),
+            'product_id' => $this->integer()->unsigned(),
+            'user_id' => $this->integer()->unsigned(),
         ], $this->tableOptions);
 
 
@@ -37,9 +37,6 @@ class m170910_040208_wishlist extends Migration
         $this->createIndex('wishlist_id', WishListProducts::tableName(), 'wishlist_id');
         $this->createIndex('product_id', WishListProducts::tableName(), 'product_id');
         $this->createIndex('user_id', WishListProducts::tableName(), 'user_id');
-
-        $userModel = Yii::$app->user->identityClass;
-        $this->addForeignKey('{{%fk_wishlist_user_id}}', WishList::tableName(), 'user_id', $userModel::tableName(), 'id');
 
     }
 
