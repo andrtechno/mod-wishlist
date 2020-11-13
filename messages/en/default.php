8b850220d7f278798a8ccf9a73e8ca62281f97dd
Andrew <andrew.panix@gmail.com>
2020-11-13 23:20:53 +0200

    init

---
+++ messages/en/default.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'MODULE_NAME' => 'Favorites',
+    'MODULE_DESC' => 'Favorites',
+    'WISHLIST' => 'Favorites',
+    'BTN_WISHLIST' => 'Add to favorites',
+    'EMPTY_LIST' => 'Wishlist is empty.',
+    'MY_WISHLIST' => 'My Wishlist',
+    'ERROR_VIEW' => 'Error. No results were found for your search.',
+    'SUCCESS_ADD' => 'The item was successfully added to the wishlist.',
+    'DELETE_SUCCESS' => 'The item was successfully removed from the wishlist.',
+    'ALREADY_EXIST' => 'The item is already on the wishlist',
+];
